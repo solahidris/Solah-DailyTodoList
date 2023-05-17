372ffe8dd16605b6fd6b914f99288d1bb9425482
solahidris <emailsolah@gmail.com>
2023-05-17 13:40:15 +0800

    added delete and add button

--- src/App.js
+++ src/App.js
@@ -9,32 +9,64 @@ const supabaseKey =
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
-  const [countries, setCountries] = useState([]);
+  const [inputdata, setInputdata] = useState([]);
   useEffect(() => {
-    getCountries();
+    getInputdata();
   }, []);
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
+  async function getInputdata() {
+    const { data } = await supabase.from("inputdata").select();
+    setInputdata(data);
   }
+
+  const deleteButtonHandler = async (id) => {
+    console.log("delete button working");
+    // eslint-disable-next-line
+    const { data, error } = await supabase
+      .from("inputdata")
+      .delete()
+      .eq("id", id);
+    // .eq('id', 'someValue')
+  };
+
+  const additemHandler = async() => {
+    console.log("add button working");
+    // eslint-disable-next-line
+    const { data, error } = await supabase
+    .from('inputdata')
+    .insert([
+      { todo:'' },
+    ]);
+
+  };
 
   return (
     <>
-      <div className="bg-slate-700 h-screen">
+      <div className="bg-slate-700 h-screen p-10">
         <img
           src={logo}
           alt="react-logo"
-          className="animate-[spin_5s_linear_infinite] flex justify-center w-screen h-20"
+          className="mb-5 animate-[spin_5s_linear_infinite] flex justify-center w-screen h-20"
         ></img>
-        <h1 className="bg-slate-500 p-5 text-3xl font-bold flex justify-center">
-          App Title
+        <h1 className="rounded-lg bg-slate-500/80 p-5 text-3xl text-slate-300 font-mono font-bold flex justify-center">
+          Paa Eltit
         </h1>
-        <div className="flex justify-center pt-5 text-white">
-          <ul>
-            {countries.map((country) => (
-              <li key={country.name}>{country.name}</li>
-            ))}
-          </ul>
+        <div className="flex justify-center pt-5 text-slate-400 font-mono">
+          <div className="bg-slate-500/50 p-8 rounded-lg">
+            <ul className="bg-slate-500/20 p-3 rounded-lg">
+              {inputdata.map((list) => (
+                <li key={list.id} className="flex">
+                  <span>{list.todo}</span>
+                  <button
+                    className="rounded-full bg-red-500/90 text-white px-[7px] h-full ml-3"
+                    onClick={() => deleteButtonHandler(list.id)}
+                  >
+                    -
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <button className="rounded-full bg-slate-500/90 text-white text-xs py-1 px-3 mt-7" onClick={additemHandler}>add</button>
+          </div>
         </div>
       </div>
     </>
