9cdbd8bc24753da849d8fce6f63e12a6a0413148
solahidris <emailsolah@gmail.com>
2023-05-21 00:15:00 +0800

    added files to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/App.js", "./src/components/reactLogo.js",
+    "./src/App.js",
+    "./src/components/reactLogo.js",
+    "./src/components/AddButton.js",
+    "./src/components/ClearButton.js",
+    "./src/components/DeleteListButton.js",
+    "./src/components/EditDoneListButton.js",
+    "./src/components/EditListButton.js",
+    "./src/components/header.js",
+    "./src/components/InputEdit.js",
+    "./src/components/reactLogo.js",
   ],
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
-}
+};
